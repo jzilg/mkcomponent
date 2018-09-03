82a992e6833e83998af46d48c7464d8459e42631
jzilg <jzg@dasburo.com>
2018-09-03 15:15:57 +0200

    remove semicolons

--- boilerplate/component.jsx
+++ boilerplate/component.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './%scss';
+import React from 'react'
+import PropTypes from 'prop-types'
+import './%scss'
 
 class %component extends React.Component {
     render() {
-        return ();
+        return ()
     }
 }
 
 %component.propTypes = {
 
-};
+}
 
-export default %component;
+export default %component
