d60b4be0c235c55c95b1e5a970ec9d5a44cce174
Johannes Zilg <johannes.zilg@gmail.com>
2018-11-29 09:55:25 +0100

    always pass props into base constructor
    pass props into base constructor, as react docs recommends

--- boilerplate/component.jsx
+++ boilerplate/component.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import './%scss'
 
 class %component extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
     }
 
     render() {
